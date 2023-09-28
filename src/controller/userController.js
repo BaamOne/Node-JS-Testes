2f8ad40ce9786b355e0d8faeaf2c680c6514689d
Fernando Matheus Engler <58644766+BaamOne@users.noreply.github.com>
2023-09-28 19:23:31 -0300

    Testes

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,11 +4,11 @@ const dataBaseConfig = require('../config/database.js');
 
 async function CreateUser(req, res){
     try {
-        const { PASSWORD, NAME} = req.query;
+        const { PASSWORD, NAME, EMAIL} = req.query;
         const connection = await mysql.createConnection(dataBaseConfig);
-        const insertUser = ` INSERT INTO USER (PASSWORD,NAME) VALUES (?,?) `;
+        const insertUser = ` INSERT INTO USER (PASSWORD,NAME,EMAIL) VALUES (?,?,?) `;
 
-        await connection.query(insertUser, [PASSWORD, NAME]);
+        await connection.query(insertUser, [PASSWORD, NAME, EMAIL]);
         await connection.end();
 
         res.status(201).send({ message: 'User created successfully!' });
