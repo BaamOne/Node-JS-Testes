6e057fdbcbf0ec6d38210f521dc1533bb8895588
Fernando Matheus Engler <58644766+BaamOne@users.noreply.github.com>
2023-09-28 21:29:22 -0300

    Adiciona novas requisições user

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -22,4 +22,58 @@ async function CreateUser(req, res){
 
 }
 
-module.exports = { CreateUser };
+async function GetAllUsers(req, res){
+    try{
+        const connection = await mysql.createConnection(dataBaseConfig);
+        const [rows] = await connection.query(`SELECT name, email, password FROM USER`);
+        await connection.end();
+        res.status(200).json(rows);
+
+    }catch(error){
+        res.status(500).send({
+            message: 'Erro getting users!',
+            body: error.message
+        });
+    }
+
+}
+
+
+async function GetUserById(req, res){
+    const { id } = req.params;
+    try{
+        const connection = await mysql.createConnection(dataBaseConfig);
+        const [user] = await connection.query(`SELECT name, email, password FROM USER WHERE id = ?`, [id]);
+        await connection.end();
+        res.status(200).json(user);
+    }catch(error){
+        res.status(500).send({
+            message: 'Erro getting user by id!',
+            body: error.message
+        });
+    }
+
+}
+
+async function UpdateUser(req,res){
+    const { NAME, PASSWORD}  = req.query;
+    const { id } = req.params;
+
+    try{
+        const connection = await mysql.createConnection(dataBaseConfig);
+        await connection.query(`UPDATE USER SET NAME = ?, PASSWORD = ? WHERE id = ?`, [NAME, PASSWORD, id]);
+        await connection.end();
+        res.status(200).json({ message: 'User updated successfully!'});
+    }catch(error){
+        res.status(500).send({
+            message: 'Erro updating user!',
+            body: error.message
+        });
+    }
+
+
+}
+
+
+
+module.exports = { CreateUser, GetAllUsers, GetUserById, UpdateUser};
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -5,4 +5,17 @@ const userController = require('../controller/userController.js');
 router.post('/users', (req, res)=>{
     userController.CreateUser(req, res);
 });
+
+router.get('/users', (req, res)=>{
+    userController.GetAllUsers(req, res);
+});
+
+router.get('/users/:id', (req, res)=>{
+    userController.GetUserById(req, res);
+});   
+
+router.put('/users/:id', (req, res)=>{
+    userController.UpdateUser(req, res);
+});
+
 module.exports = router;
